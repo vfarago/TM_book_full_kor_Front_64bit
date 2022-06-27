48b6043f7eb0d838313018bbad96df90dc8357c1
VP-developer <6494878+vfarago@users.noreply.github.com>
2022-06-27 12:33:16 +0900

    Kevin_0627
    복사만 완료.

---
+++ Assets/My/Scripts/CoverTrackerbleEventHandler.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class CoverTrackerbleEventHandler : TrackableEventHandler
+{
+    public string targetName;
+
+    private bool isEndAR;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        targetName = mTrackableBehaviour.TrackableName.ToLower();
+    }
+
+    protected override void OnTrackingFound()
+    {
+        isEndAR = prefabLoader.isEndAR;
+
+        if (!isEndAR && !canvasManager.isCoverTarget)
+        {
+            Debug.Log("        found " + targetName);
+            canvasManager.isCoverTarget = true;
+            prefabLoader.isTargetoff = true;
+            canvasManager.OnTargetOffObject(true);
+        }
+    }
+}
